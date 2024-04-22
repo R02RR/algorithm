f97f9570e28db6ec1201ea7951e6ca673ac24144
R02RR <1530919150@qq.com>
2024-04-22 21:41:49 +0800

    climbStairs

---
+++ git/src/爬楼梯.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class 爬楼梯 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+
+
+    }
+
+    public static int climbStairs(int n) {
+        if(n == 1){
+            return 1;
+        }
+        if(n == 2){
+            return 2;
+        }
+        if(n == 0)
+            return 0;
+        return climbStairs(n-1) + climbStairs(n - 2);
+    }
+}
